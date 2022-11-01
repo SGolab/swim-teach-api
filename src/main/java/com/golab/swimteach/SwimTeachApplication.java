d54126953834ed246634c8377d9e587424e667f8
SGolab <szym.golab@gmail.com>
2022-11-01 22:00:59 +0100

    Initial commit.

---
+++ src/main/java/com/golab/swimteach/SwimTeachApplication.java
@@ -0,0 +1,13 @@
+package com.golab.swimteach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwimTeachApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwimTeachApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/golab/swimteach/SwimTeachApplicationTests.java
@@ -0,0 +1,13 @@
+package com.golab.swimteach;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwimTeachApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
