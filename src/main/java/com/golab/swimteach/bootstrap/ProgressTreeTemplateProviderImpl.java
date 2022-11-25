88bfc528db7d21405e01d4ba0ecbb812273d74ef
SGolab <szym.golab@gmail.com>
2022-11-25 21:15:31 +0100

    Provided TREE_TEMPLATE_PATH through application.properties instead of hardcode.

--- src/main/java/com/golab/swimteach/bootstrap/ProgressTreeTemplateProviderImpl.java
+++ src/main/java/com/golab/swimteach/bootstrap/ProgressTreeTemplateProviderImpl.java
@@ -2,6 +2,7 @@ package com.golab.swimteach.bootstrap;
 
 import com.golab.swimteach.dto.ProgressTreeTemplate;
 import com.google.gson.Gson;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 
@@ -12,9 +13,15 @@ import java.nio.file.Files;
 @Component
 public class ProgressTreeTemplateProviderImpl implements ProgressTreeTemplateProvider {
 
+    private final String TREE_TEMPLATE_PATH;
+
+    public ProgressTreeTemplateProviderImpl(@Value("${progressTree.templatePath}") String treeTemplatePath) {
+        TREE_TEMPLATE_PATH = treeTemplatePath;
+    }
+
     @Override
     public ProgressTreeTemplate getProgressTreeTemplate() {
-        ClassPathResource resource = new ClassPathResource("/progress-tree-template.json");
+        ClassPathResource resource = new ClassPathResource(TREE_TEMPLATE_PATH);
 
         try {
             String json = Files.readString(resource.getFile().toPath(), Charset.defaultCharset());
