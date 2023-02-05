0c66a0640198d168c8e939bb6fa1ab14b0941796
SGolab <szym.golab@gmail.com>
2023-02-06 00:27:05 +0100

    Added /userDetails endpoint for fetching own data.

--- src/main/java/com/golab/swimteach/controllers/UserDetailsController.java
+++ src/main/java/com/golab/swimteach/controllers/UserDetailsController.java
@@ -3,30 +3,32 @@ package com.golab.swimteach.controllers;
 import com.golab.swimteach.dto.UserDetailsDto;
 import com.golab.swimteach.mapper.UserDetailsMapper;
 import com.golab.swimteach.model.User;
+import com.golab.swimteach.repositories.UserRepository;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Objects;
-
 @RestController
 public class UserDetailsController {
 
-    private final UserDetailsService userDetailsService;
+    private final UserRepository userRepository;
     private final UserDetailsMapper userDetailsMapper = UserDetailsMapper.getInstance();
 
-    public UserDetailsController(UserDetailsService userDetailsService) {
-        this.userDetailsService = userDetailsService;
+    public UserDetailsController(UserRepository userRepository) {
+        this.userRepository = userRepository;
     }
 
-    @GetMapping("/users/{userName}/details")
-    public UserDetailsDto getUserDetails(@AuthenticationPrincipal User user, @PathVariable String userName) throws Exception {
+    @GetMapping("/userDetails")
+    public UserDetailsDto getUserDetails(@AuthenticationPrincipal User user) throws Exception {
+        return userDetailsMapper.toUserDetailsDto(user);
+    }
 
-        if (!Objects.equals(user.getUsername(), userName)) {
-            throw new Exception("User with username: " + userName + " was not found"); //todo exceptions
-        }
+    @GetMapping("/users/{userId}/userDetails")
+    public UserDetailsDto getUserDetails(@PathVariable Long userId) {
+
+        User user = userRepository.findById(userId).orElseThrow(() -> new RuntimeException("User with id: " + userId + " was not found."));
 
         return userDetailsMapper.toUserDetailsDto(user);
     }
